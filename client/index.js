797692e022baf8d779b3c10fbb0efa6da3520451
luck_zr <529130510@qq.com>
2016-09-19 08:44:26 +0800

    add sth

--- client/index.js
+++ client/index.js
@@ -2,3 +2,18 @@
  * Created by zhangran on 16/9/9.
  */
 
+import React from "react";
+import {browserHistory} from "react-router";
+import {syncHistoryWithStore} from "react-router-redux";
+import configureStore from "./redux/store/index";
+import Root from "./Root";
+
+const store = configureStore()
+const history = syncHistoryWithStore(browserHistory, store)
+const root = (<Root history={history} store={store}/>)
+
+ReactDOM.render(root, document.getElementById('root'))
+
+if (module.hot) {
+  module.hot.accept();
+}
